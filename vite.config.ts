3d11472a0e5d8916bf1490b34c224db12fe880a9
PhoenixbgOrg <37903079+PhoenixbgOrg@users.noreply.github.com>
2026-01-26 20:41:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Важно: Това позволява сайтът да работи в подпапка на GitHub (https://user.github.io/repo/)
+  base: '/TechKids-BG-/',  // точно с тирето и големи/малки букви както е repo-то!
 })
